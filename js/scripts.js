a30c85b7b8dfd69a4555419996681655313f157e
Lois Choi <loisch22@gmail.com>
2017-07-14 11:02:14 -0700

    add if else statements that change numbers divisibly by 5 to 'pong' and numbers divisible by 15 to 'ping-pong' - currently working on error alerts

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,17 @@
 var pingPong = function(inputNumber) {
   var copyNumber = inputNumber;
   var numberOutput = [];
-debugger;
+
   for (var i = 1; i <= copyNumber; i++) {
-    if (i % 3 === 0) {
-      var ping = [i].slice();
-      numberOutput.push(ping = '"ping"');
+    if (i % 15 === 0) {
+      var pingPong = [i].slice();
+      numberOutput.push(pingPong = '"ping-pong"');
     } else if (i % 5 === 0) {
       var pong = [i].slice();
-      numberOutput.push(pong = '"pong"'); 
+      numberOutput.push(pong = '"pong"');
+    } else if (i % 3 === 0) {
+      var ping = [i].slice();
+      numberOutput.push(ping = '"ping"');
     } else if (copyNumber[i] === copyNumber) {
       numberOutput.push(i);
     } else {

1fb9c6eb40696ff71e139316b9908c37fe327c6d
Lois Choi <loisch22@gmail.com>
2017-07-14 09:10:25 -0700

    work on adding the first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-var copyInput =
-
 var pingPong = function(inputNumber) {
+  var copyInput = inputNumber;
+
+  for (var i = 1; i <= inputNumber; i++) {
+
+  }
 
 
-
-}
+};
 
 
 
@@ -19,7 +21,7 @@ var pingPong = function(inputNumber) {
 
 $(document).ready(function() {
   $("form#pingForm").submit(function() {
-    var inputNumber = parseInt($("input#number").val());
+    var inputNumber = $("input#number").val();
     var resultOutput = pingPong(inputNumber);
     $("ul#listOutput").append("<li>" + resultOutput + "</li>");
     $("#results").show();
